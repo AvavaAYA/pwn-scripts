e3be3dc2b38d7abf60c73b8176d6be50d5732253
AvavaAYA <nana7777777mi@gmail.com>
2022-03-23 18:58:10 +0800

    Updating basic template

---
+++ .pwn_template.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+from pwn import *
+
+context.log_level = 'debug'
+context.arch='amd64'
+context.terminal = ['tmux','sp','-h','-l','120']
+
+# remote_service = ""
+# remote_service = remote_service.strip().split(" ")
+# p = remote(remote_service[0], int(remote_service[1]))
+filename = "./pwn"
+p = process(filename)
+e = ELF(filename, checksec=False)
+l = ELF(e.libc.path, checksec=False)
+
+rl = lambda a=False : p.recvline(a)
+ru = lambda a,b=True : p.recvuntil(a,b)
+rn = lambda x : p.recvn(x)
+sn = lambda x : p.send(x)
+sl = lambda x : p.sendline(x)
+sa = lambda a,b : p.sendafter(a,b)
+sla = lambda a,b : p.sendlineafter(a,b)
+irt = lambda : p.interactive()
+dbg = lambda text=None : gdb.attach(p, text)
+lg = lambda s : log.info('\033[1;31;40m %s --> 0x%x \033[0m' % (s, eval(s)))
+uu32 = lambda data : u32(data.ljust(4, b'\x00'))
+uu64 = lambda data : u64(data.ljust(8, b'\x00'))
+
+
+
+
+irt()
