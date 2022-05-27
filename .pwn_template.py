302ef538ba263992b96df7e537567e6848608dc5
AvavaAYA <nana7777777mi@gmail.com>
2022-05-28 07:50:51 +0800

    updating

--- .pwn_template.py
+++ .pwn_template.py
@@ -7,7 +7,7 @@ context.arch='amd64'
 context.terminal = ['tmux','sp','-h','-l','120']
 
 # remote_service = ""
-# remote_service = remote_service.strip().split(" ")
+# remote_service = remote_service.strip().split(":")
 # p = remote(remote_service[0], int(remote_service[1]))
 filename = "./pwn"
 p = process(filename)
@@ -24,10 +24,16 @@ sla = lambda a,b : p.sendlineafter(a,b)
 irt = lambda : p.interactive()
 dbg = lambda text=None : gdb.attach(p, text)
 lg = lambda s : log.info('\033[1;31;40m %s --> 0x%x \033[0m' % (s, eval(s)))
+i2b = lambda c : str(c).encode()
 uu32 = lambda data : u32(data.ljust(4, b'\x00'))
 uu64 = lambda data : u64(data.ljust(8, b'\x00'))
+def debugPID():
+	lg("p.pid")
+	input()
+	pass
 
 
 
 
+debugPID()
 irt()
