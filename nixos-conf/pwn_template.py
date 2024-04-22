94f5d027a1b071aab6ee8cfff1b04eee44d8677e
avavaaya <nana7777777mi@gmail.com>
2024-04-22 15:36:12 +0800

    updating

--- nixos-conf/pwn_template.py
+++ nixos-conf/pwn_template.py
@@ -1,14 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #  author: @eastXueLian
 
 from pwn import *
-from lianpwn.nocli import *
+import lianpwn
 import sys
 
 context.log_level = "debug"
 context.arch = "amd64"
 context.terminal = ["tmux", "sp", "-h", "-l", "120"]
+
+rl = lambda a=False: io.recvline(a)
+ru = lambda a, b=True: io.recvuntil(a, b)
+rn = lambda x: io.recvn(x)
+s = lambda x: io.send(x)
+sl = lambda x: io.sendline(x)
+sa = lambda a, b: io.sendafter(a, b)
+sla = lambda a, b: io.sendlineafter(a, b)
+ia = lambda: io.interactive()
+dbg = lambda text=None: gdb.attach(io, text)
+i2b = lambda c: str(c).encode()
+u32_ex = lambda data: u32(data.ljust(4, b"\x00"))
+u64_ex = lambda data: u64(data.ljust(8, b"\x00"))
 
 LOCAL = 1
 filename = "./pwn"
@@ -20,9 +33,6 @@ else:
     io = remote(remote_service[0], int(remote_service[1]))
 elf = ELF(filename, checksec=False)
 libc = ELF(elf.libc.path, checksec=False)
-
-
-debugPID()
 
 
 ia()
